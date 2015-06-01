f2c7b7c840a23965a3a39c951c050bf5cceac321
chris carneiro <christ.carneiro@gmail.com>
2015-06-01 18:40:31 +0200

    WifiScanResultReceiver, required wifi permission added

--- app/src/main/java/net/opencurlybraces/android/projects/wifihandler/ConfiguredWifiListActivity.java
+++ app/src/main/java/net/opencurlybraces/android/projects/wifihandler/ConfiguredWifiListActivity.java
@@ -1,6 +1,5 @@
 package net.opencurlybraces.android.projects.wifihandler;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
---
+++ app/src/main/java/net/opencurlybraces/android/projects/wifihandler/receiver/WifiScanResultsReceiver.java
@@ -0,0 +1,24 @@
+package net.opencurlybraces.android.projects.wifihandler.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * {@link BroadcastReceiver} filtered on {@link android.net.wifi
+ * .WifiManager#SCAN_RESULTS_AVAILABLE_ACTION}
+ * system action.
+ *
+ * @author Chris Carneiro
+ */
+public class WifiScanResultsReceiver extends BroadcastReceiver {
+    private static final String TAG = WifiScanResultsReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //TODO start a service to handle the scan results
+
+        Log.d(TAG, "SCAN_RESULTS received");
+    }
+}
