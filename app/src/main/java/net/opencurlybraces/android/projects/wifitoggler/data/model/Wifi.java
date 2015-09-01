db017cec0601727333457eed4192701e04b6427d
chris carneiro <christ.carneiro@gmail.com>
2015-09-01 16:13:04 +0200

    SavedWifi model object created

---
+++ app/src/main/java/net/opencurlybraces/android/projects/wifitoggler/data/model/Wifi.java
@@ -0,0 +1,11 @@
+package net.opencurlybraces.android.projects.wifitoggler.data.model;
+
+/**
+ * {@link net.opencurlybraces.android.projects.wifitoggler.data.table.SavedWifi} Java Model Object
+ * Created by chris on 01/09/15.
+ */
+public class Wifi {
+    public String ssid;
+    public int status;
+    public boolean isAutoToggle;
+}
